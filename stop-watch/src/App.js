564fddf6821def7ae261592e1c2cb1ea2bc11615
katikisai <saiprakash.katiki@gmail.com>
2021-09-02 18:08:19 +0530

    Timer

---
+++ stop-watch/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TimerClass from './timer-class';
+
+function App() {
+  return (
+    <div className="App">
+      <TimerClass />
+    </div>
+  );
+}
+
+export default App;
